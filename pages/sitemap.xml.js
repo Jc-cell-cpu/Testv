e108c1edbbd73779ce009c92b5b048b1df07f95b
jc <sdmohapatra2000@gmail.com>
2024-04-01 15:54:55 +0530

    sitemap changed

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,32 @@
+// pages/sitemap.xml.js
+
+const homepage = "https://www.capitaltaxplus.com"
+
+function generateSiteMap() {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>${homepage}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+      </url>
+      <url>
+        <loc>${homepage}/Pricing</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+      </url>
+    </urlset>
+  `;
+}
+
+export async function getServerSideProps({ res }) {
+  const sitemap = generateSiteMap();
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default function SiteMap() {}
