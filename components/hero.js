430b6a36d48937f8159a6cc453cdc8967fd156fe
Srichandan Mohapatra <79102175+Jc-cell-cpu@users.noreply.github.com>
2024-04-16 00:40:45 +0530

    change size

--- components/hero.js
+++ components/hero.js
@@ -60,7 +60,7 @@ const Hero = () => {
       <Container className="flex flex-wrap ">
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
-            <h1 className="text-5xl font-bold leading-snug tracking-tight text-gray-800 lg:text-5xl lg:leading-tight xl:text-7xl xl:leading-tight dark:text-white">
+            <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
             Save Taxes; 
             <br/>
             <span className="bg-gradient-to-r from-purple-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
